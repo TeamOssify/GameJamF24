a699ff926016dd27c3164b1ccdcbd7c4b53ba8da
SanctumSpear <146847788+SanctumSpear@users.noreply.github.com>
2024-10-22 23:42:10 -0400

    Overhaul to how UI is loaded

--- Assets/Scripts/UIHandlers/LocationHandler.cs
+++ Assets/Scripts/UIHandlers/LocationHandler.cs
@@ -4,34 +4,22 @@ using UnityEngine.SceneManagement;
 public class LocationHandler : MonoBehaviour {
     private string _currentScene;
     private string _mapScene = "MainGame";
-   
+
     void Start() {
-        _currentScene = _mapScene;
-        SceneManager.LoadSceneAsync(4, LoadSceneMode.Additive);
+        _currentScene = SceneManager.GetActiveScene().name;
+        Debug.Log($"Starting");
     }
 
     public void ChangeScene(string sceneName) {
-        
-
-        if (_currentScene != null) {
-            SceneManager.UnloadSceneAsync(_currentScene);
-            Debug.Log($"Unloading:" + _currentScene);
-        }
-
-        Debug.Log($"Loading scene: {sceneName}");
-        SceneManager.LoadSceneAsync(sceneName);
-
-        _currentScene = sceneName;
+        SceneManager.LoadSceneAsync( sceneName );
     }
 
     public void Back() {
         if (SceneManager.GetActiveScene().name != null) {
-            SceneManager.UnloadSceneAsync(SceneManager.GetActiveScene().name);
 
             SceneManager.LoadSceneAsync(_mapScene);
-            Debug.Log($"Returning to:" + _mapScene);
+            Debug.Log($"Returning to: " + _mapScene);
+
         }
-
-
     }
 }
