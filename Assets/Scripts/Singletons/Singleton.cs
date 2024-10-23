048616feddb7d6f8ef80b6519a0467db85ba4067
ModularMemory <182370532+ModularMemory@users.noreply.github.com>
2024-10-23 16:01:10 -0400

    Use DDOL instead of hide flags

--- Assets/Scripts/Singletons/Singleton.cs
+++ Assets/Scripts/Singletons/Singleton.cs
@@ -8,10 +8,10 @@ public abstract class Singleton<T> : MonoBehaviour where T : Component {
             if (!_instance) {
                 var obj = new GameObject {
                     name = typeof(T).Name,
-                    hideFlags = HideFlags.HideAndDontSave,
                 };
 
                 _instance = obj.AddComponent<T>();
+                DontDestroyOnLoad(obj); // DDOL instead of hide flags so it shows in the editor
             }
 
             return _instance;
