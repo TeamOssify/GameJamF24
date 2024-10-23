eb6ddf1027d1647a12acea04a5b29d7927fbf6ec
SanctumSpear <146847788+SanctumSpear@users.noreply.github.com>
2024-10-23 00:12:23 -0400

    Add money UI frontend

--- Assets/Scripts/UIHandlers/MoneyUI.cs
+++ Assets/Scripts/UIHandlers/MoneyUI.cs
@@ -1,18 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class MoneyUI : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    public TextMeshProUGUI moneyText;
+
+    private void Start() {
+        var Bank = BankAccountManager.Instance;
+        moneyText.text = $"$: {Bank.Balance}";
+
+        Bank.BalanceChanged.AddListener(OnBalanceChanged);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    private void OnDestroy() {
+        BankAccountManager.Instance.BalanceChanged.RemoveListener(OnBalanceChanged);
+    }
+
+    private void OnBalanceChanged(MetricChangedArgs args) {
+        moneyText.text = $"$: {(float)args.NewValue}";
     }
 }
