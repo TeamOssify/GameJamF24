b590f11ff782989af911f1ceaf2bb594fafdeef2
Simonatoratorator <113464762+Simonatoratorator@users.noreply.github.com>
2024-10-24 12:00:54 -0400

    Added Dialogue Manager
    I added a dialogue manager to each scene

---
+++ Assets/Scripts/DialogueManager.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class DialogueManager : MonoBehaviour
+{
+    [SerializeField]
+    private TMP_Text textField;
+
+    public TextAsset inputTextFile;
+
+    private string entireText;
+    private List<string> line;
+    private int lineNum = 1;
+
+
+    void Start()
+    {
+        entireText = inputTextFile.text;
+        line = new List<string>();
+        line.AddRange(entireText.Split("\n"));
+        Debug.Log(line[4]);
+
+        textField.text = line[0];
+    }
+
+    public void nextText() {
+        textField.text = line[lineNum];
+        lineNum++;
+    }
+}
