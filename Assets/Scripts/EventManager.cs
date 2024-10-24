5f516561cec6c2550e085d1384e17b1f84c784a1
Simonatoratorator <113464762+Simonatoratorator@users.noreply.github.com>
2024-10-24 15:49:26 -0400

    Added events
    I made actual events in eventManager

--- Assets/Scripts/EventManager.cs
+++ Assets/Scripts/EventManager.cs
@@ -26,31 +26,43 @@ public class EventManager : MonoBehaviour {
     }
 
     public Event[] mapRandomEvents = {
-        new Event("map1", 0.9, 60, 30, 0, 10, 0),
-        new Event("map2", 0.9, 20, 120, -10, -10, -50),
-        new Event("map3", 0.9, 50, 1, 0, 5, -1),
-        new Event("map4", 0.9, 10, 360, -80, -50, -10000)
+        new Event("Friend", 1, 60, 30, 0, 10, 0),
+        new Event("Beggar", 0.9, 40, 5, 0, 5, -1),
+        new Event("Robbery", 0.7, 40, 1, 0, 5, -1),
+        new Event("Truck", 0.5, 10, 360, -80, -50, -10000),
+        new Event("Spooked", 0.3, 80, 10, -10, -20, 0),
+        new Event("Panic", 0.2, 60, 30, -20, -30, 0),
+        new Event("Heart Attack", 0.1, 10, 360, -50, -20, 0)
     };
 
     public Event[] houseRandomEvents = {
-        new Event("house1", 0.9, 60, 30, 0, 10, 0),
-        new Event("house2", 0.9, 20, 120, -10, -10, -50),
-        new Event("house3", 0.9, 50, 1, 0, 5, -1),
-        new Event("house4", 0.9, 10, 360, -80, -50, -10000)
+        new Event("D2D Salesman", 0.9, 30, 15, 0, -5, -36),
+        new Event("Pests", 0.9, 50, 5, -1, -5, 0),
+        new Event("Spooky Pests", 0.5, 60, 30, -5, -15, -15),
+        new Event("Broken Chair", 0.4, 20, 60, -20, -20, -20),
+        new Event("Spooked", 0.3, 80, 10, -10, -20, 0),
+        new Event("Panic", 0.2, 60, 30, -20, -30, 0),
+        new Event("Heart Attack", 0.1, 10, 360, -50, -20, 0)
     };
 
     public Event[] workRandomEvents = {
-        new Event("work1", 0.9, 60, 30, 0, 10, 0),
-        new Event("work2", 0.9, 20, 120, -10, -10, -50),
-        new Event("work3", 0.9, 50, 1, 0, 5, -1),
-        new Event("work4", 0.9, 10, 360, -80, -50, -10000)
+        new Event("Annoying Worker", 1, 70, 10, 0, -10, 0),
+        new Event("Broken Printer", 0.8, 50, 30, -1, -15, -50),
+        new Event("Extra Work", 0.9, 50, 60, 0, -5, 18),
+        new Event("Nice Worker", 1, 70, 10, 0, 15, 0),
+        new Event("Spooked", 0.3, 80, 10, -10, -20, 0),
+        new Event("Panic", 0.2, 60, 30, -20, -30, 0),
+        new Event("Heart Attack", 0.1, 10, 360, -50, -20, 0)
     };
 
     public Event[] clubRandomEvents = {
-        new Event("club1", 0.9, 60, 30, 0, 10, 0),
-        new Event("club2", 0.9, 20, 120, -10, -10, -50),
-        new Event("club3", 0.9, 50, 1, 0, 5, -1),
-        new Event("club4", 0.9, 10, 360, -80, -50, -10000)
+        new Event("Free Drink", 1, 30, 5, -5, 30, 0),
+        new Event("Drink", 1, 50, 5, -5, 30, -5),
+        new Event("Blackout", 0.5, 30, 180, -20, -20, -20),
+        new Event("Kicked Out", 0.4, 10, 30, -5, -20, -15),
+        new Event("Spooked", 0.3, 80, 10, -10, -20, 0),
+        new Event("Panic", 0.2, 60, 30, -20, -30, 0),
+        new Event("Heart Attack", 0.1, 10, 360, -50, -20, 0)
     };
 
     public void RandomEvent() {
